85f02cc7f48b3d35f1f6f76365bd1f0fd61e6d89
dilsoncampelo10 <dilsono316@gmail.com>
2023-06-04 20:06:45 -0300

    model supplier

---
+++ src/main/java/com/infotech/info/models/Supplier.java
@@ -0,0 +1,23 @@
+package com.infotech.info.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 10)
+    private String acronym;
+
+    @Column
+    private String locality;
+}
