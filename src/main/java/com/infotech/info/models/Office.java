97a5757f3af865b13076d1011ce6cb507c89ce8f
dilsoncampelo10 <dilsono316@gmail.com>
2023-06-04 19:10:47 -0300

    create office model

--- src/main/java/com/infotech/info/models/Office.java
+++ src/main/java/com/infotech/info/models/Office.java
@@ -5,21 +5,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Phone {
-
+public class Office {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
-    private String number;
+    private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "client_id", nullable = false, updatable = false)
-    private Client client;
+    @Column(columnDefinition = "TEXT")
+    private String description;
 
 }
